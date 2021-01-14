0aabd4587c13e22632c3c025d976d27fb8eca68d
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2021-01-15 02:55:07 +0530

    python program

---
+++ IfElse.py
@@ -0,0 +1,15 @@
+a=1001
+b=1002
+
+if a>b:
+    print("a is grater")
+elif a<b:
+    print("b is greater")
+else:
+    print("both r equal")
+
+print("a is bigger") if a>b else print("b is bada") if a<b else print("equal")
+
+
+if a>b:
+    pass
