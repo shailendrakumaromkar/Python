69fd175fa9815bec1d4b8a07997c7cb027e60904
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2021-01-15 03:01:48 +0530

    python program

---
+++ While.py
@@ -0,0 +1,15 @@
+x=1
+while x<10:
+    #print(x)
+    if x==5:
+            break
+    x+=1
+
+while x<10:
+    x+=1
+    if x==6:
+        continue
+    print(x)
+
+else:
+    print("in else block")
