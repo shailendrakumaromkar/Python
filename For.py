160a434311a3fc2c8567453dd223408c8bb411ad
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2021-01-15 03:19:22 +0530

    python program

---
+++ For.py
@@ -0,0 +1,31 @@
+name=["raja","kumar","omkar"]
+for i in name:
+    print(i)
+
+for i in "raja":
+    print(i)
+
+for i in name:
+    print(i)
+    if i=="kumar":
+        break
+
+for i in name:
+    print(i)
+    if i=="kumar":
+        continue
+
+for i in range(1):
+    print(i)
+
+for i in range(2,9):
+    print(i)
+
+for i in range(0,100,25):
+    print(i)
+
+int=["first","middle","last"]
+
+for i in name:
+    for j in int:
+        print(1,j)
